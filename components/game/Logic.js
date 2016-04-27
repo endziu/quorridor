adc0679d20628cd20a89c4d83fc1b120c39225c6
endziu <koper.andrzej@gmail.com>
2016-04-27 23:17:34 +0200

    isWallValid check

--- components/game/Logic.js
+++ components/game/Logic.js
@@ -1,22 +1,26 @@
+// (Wall, Wall) => bool;        
+const overlapH = (w1,w2) => (w1.pos.y === w2.pos.y) && (w1.pos.x - w2.pos.x ===  1 || w1.pos.x - w2.pos.x === -1);
+const overlapV = (w1,w2) => (w1.pos.x === w2.pos.x) && (w1.pos.y - w2.pos.y ===  1 || w1.pos.y - w2.pos.y === -1);
+const isPosEqual = (w1,w2) => w1.pos.x === w2.pos.x && w1.pos.y === w2.pos.y;
+const isDiffType = (w1,w2) => w1.type  !== w2.type;
+const isSameType = (w1,w2) => w1.type  === w2.type;
+
 module.exports = {
+  // (Wall, GameState) => bool;
   isWallValid: (wall, state) => {
-    return state.walls.filter((w) => {
-      if (wall.type === "horizontal" && w.type === "vertical") {
-        return  (w.pos.y === wall.pos.y &&
-                (w.pos.x === wall.pos.x));
-      } else if (wall.type === "vertical" && w.type === "horizontal") {
-         return (w.pos.x === wall.pos.x &&
-                (w.pos.y === wall.pos.y));
-      } else if (wall.type === w.type && wall.type === "horizontal") {
-         return ((w.pos.y === wall.pos.y && w.pos.x === wall.pos.x) ||
-                (w.pos.y === wall.pos.y && w.pos.x - wall.pos.x === 1) ||
-                (w.pos.y === wall.pos.y && w.pos.x - wall.pos.x === -1));
-      } else if (wall.type === w.type && wall.type === "vertical") {
-         return ((w.pos.x === wall.pos.x && w.pos.y === wall.pos.y) ||
-                (w.pos.x === wall.pos.x && w.pos.y - wall.pos.y === 1) ||
-                (w.pos.x === wall.pos.x && w.pos.y - wall.pos.y === -1));
-      }
-    }).length === 0;
+    // (Wall) => bool;
+    const blockingWalls = (w) => {
+      if (isDiffType(w, wall)) {
+        return isPosEqual(w, wall);
+      } else if (isSameType(w, wall)) {
+        if (wall.type === "horizontal") {
+          return overlapH(w,wall);
+        } else if (wall.type === "vertical") {
+          return overlapV(w,wall);
+        }
+      } 
+    }
+    return state.walls.filter(blockingWalls).length === 0;
   },
   isMoveValid: (move, state) => {
     // body...
