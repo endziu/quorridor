96ceba63a9a7e417c35dc983d2471cce06225c5c
endziu <koper.andrzej@gmail.com>
2016-04-28 13:29:00 +0200

    refactoring

--- components/game/Logic.js
+++ components/game/Logic.js
@@ -1,15 +1,14 @@
-const overlapH = (w1,w2) => (w1.pos.y === w2.pos.y) && (w1.pos.x - w2.pos.x ===  1 || w1.pos.x - w2.pos.x === -1);
-const overlapV = (w1,w2) => (w1.pos.x === w2.pos.x) && (w1.pos.y - w2.pos.y ===  1 || w1.pos.y - w2.pos.y === -1);
-const isPosEqual = (w1,w2) => w1.pos.x === w2.pos.x && w1.pos.y === w2.pos.y;
-const isDiffType = (w1,w2) => w1.type  !== w2.type;
-const isSameType = (w1,w2) => w1.type  === w2.type;
+const isPosEqual = (w1,w2) =>  w1.pos.x === w2.pos.x  && w1.pos.y === w2.pos.y;
+const isDiffType = (w1,w2) =>  w1.type  !== w2.type;
+const overlapH   = (w1,w2) => (w1.pos.y === w2.pos.y) && (w1.pos.x - w2.pos.x ===  1 || w1.pos.x - w2.pos.x === -1);
+const overlapV   = (w1,w2) => (w1.pos.x === w2.pos.x) && (w1.pos.y - w2.pos.y ===  1 || w1.pos.y - w2.pos.y === -1);
 
 module.exports = {
   isWallValid: (wall, state) => {
     const blockingWalls = (w) => {
       if (isDiffType(w, wall)) {
         return isPosEqual(w, wall);
-      } else if (isSameType(w, wall)) {
+      } else {
         if (wall.type === "horizontal") {
           return isPosEqual(w,wall) || overlapH(w,wall);
         } else if (wall.type === "vertical") {
